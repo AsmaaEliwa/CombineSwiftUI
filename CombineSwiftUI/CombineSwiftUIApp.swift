d208cc400d687e6c9afa2834d06a9831587e1f34
Asmaa Eliwa <asmaagamalelsayed96@gmail.com>
2024-04-16 12:39:38 -0400

    setting up app

---
+++ CombineSwiftUI/CombineSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineSwiftUIApp.swift
+//  CombineSwiftUI
+//
+//  Created by asmaa gamal  on 16/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct CombineSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
